e96a6c66e666ef1a0203970b5d95c009fb10ccc6
albertkombol <kombolalbert@gmail.com>
2022-01-26 16:11:26 +0100

    update code

--- js/main.js
+++ js/main.js
@@ -12,42 +12,42 @@ class Converter {
         if (!this.amount) {
             this.output(msg, 'err-amount');
             return;
-        } else if (!this.from ) {
+        } else if (!this.from) {
             this.output(msg, 'err-from');
             return;
-        }else if(!this.to) {
+        } else if (!this.to) {
             this.output(msg, 'err-to');
             return;
-        }else{
+        } else {
             let msg = '';
             console.log('good', this.amount);
             this.output(msg, 'err-amount');
             this.output(msg, 'err-from');
             this.output(msg, 'err-to');
             this.convert();
-            }
+        }
 
     }
 
-     //get element
-     getElement = id => document.getElementById(id);
+    //get element
+    getElement = id => document.getElementById(id);
 
     //Output
     output(msg, id) {
-        let display = this.getElement(id);
+        const display = this.getElement(id);
         display.innerText = msg;
     }
 
     //encode
-    encodeUri() {
-        let fromCurrency = encodeURIComponent(this.from);
-        let toCurrency = encodeURIComponent(this.to);
+    encodeUri = () => {
+        const fromCurrency = encodeURIComponent(this.from);
+        const toCurrency = encodeURIComponent(this.to);
         return `${fromCurrency}_${toCurrency}`;
     }
 
     //Sort JSON response in alphabetical order
     sortRes(array, key) {
-        return array.sort(function (a, b) {
+        return array.sort((a, b) => {
             const x = a[key];
             const y = b[key];
             return ((x < y) ? -1 : ((x > y) ? 1 : 0));
@@ -56,12 +56,12 @@ class Converter {
 
     //api request for currencies
     async getCurrencies() {
-        let parentFrom = this.getElement('from');
-        let parentTo = this.getElement('to');
-        let currStream = await fetch(`https://free.currconv.com/api/v7/currencies?apiKey=16ef1ff68cbc6fe7bdae`);
-        let currencies = await currStream.json();
+        const parentFrom = this.getElement('from');
+        const parentTo = this.getElement('to');
+        const currStream = await fetch(`https://free.currconv.com/api/v7/currencies?apiKey=16ef1ff68cbc6fe7bdae`);
+        const currencies = await currStream.json();
 
-        let sortedCurrs = this.sortRes(Object.values(currencies.results), 'currencyName');
+        const sortedCurrs = this.sortRes(Object.values(currencies.results), 'currencyName');
         for (const currency of sortedCurrs) {
             this.appendOptns(parentFrom, parentTo, currency);
 
@@ -76,7 +76,7 @@ class Converter {
             return txt;
         }
 
-    }   
+    }
 
     //Append currency options
     appendOptns(parentFrom, parentTo, content) {
@@ -94,29 +94,27 @@ class Converter {
     }
 
     //api request for rate
-    getRate = async () => {
-        try{
-        let currStream = await fetch(`https://free.currconv.com/api/v7/convert?q=${this.encodeUri()}&compact=ultra&apiKey=16ef1ff68cbc6fe7bdae`);
-        let rate = await currStream.json();
-        return rate;
-        }catch(e){
+    reqRate = async () => {
+        try {
+            const currStream = await fetch(`https://free.currconv.com/api/v7/convert?q=${this.encodeUri()}&compact=ultra&apiKey=16ef1ff68cbc6fe7bdae`);
+            const rate = await currStream.json();
+            return rate;
+        } catch (e) {
             console.log(e);
         }
     }
 
     //convert
-    convert() {
-        this.getRate().then(res => {
-            let rate = Object.values(res)[0];
-            let display = this.getElement('display');
-            let result = rate * this.amount;
-            display.innerText = `Exchanges to ${result.toFixed(2)}`;
-            
+    convert = () => {
+        this.reqRate().then(res => {
+            const rate = Object.values(res)[0];
+            const result = (rate * this.amount).toFixed(2);
+            this.output(`${this.amount} ${this.from} exchanges to ${result} ${this.to}`, 'display');
         }).catch(err => console.log(err));
-    
-        }
-       
-    
 
-    
+    }
+
+
+
+
 }
